9d4d864c640fb196de2f306d60b4cfa122d03238
Jacob Tingling <jacob.tingling@protonmail.com>
2021-11-17 14:09:58 -0500

    Revert "Issue #1: Updated connection."
    This reverts commit e37875a42dd6cdf0946e4104b05cadd7ae9f4a93.

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,7 @@
-const dotenv = require('dotenv');
 const Sequelize = require("sequelize");
 
-dotenv.config();
-
-const db = new Sequelize( process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false,
-  dialect: 'postgres'
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
 
 module.exports = db;
