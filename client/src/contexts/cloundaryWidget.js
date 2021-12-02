00749d454f4494860b249667f21dd1bc8100bace
Jacob Tingling <jacob.tingling@protonmail.com>
2021-12-02 16:17:21 -0500

    Moved private keys to env

--- client/src/contexts/cloundaryWidget.js
+++ client/src/contexts/cloundaryWidget.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
-export const CLOUD_NAME = "deuocoi8l";
-export const UPLOAD_PRESET = "stwli0j1";
+export const CLOUD_NAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
+export const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET;
 export const cloudinaryWidget = window.cloudinary;
 
 export const UploadWidget = React.createContext(cloudinaryWidget);
